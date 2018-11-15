04fd7754b8fe560efe43140d3dd824a724762f68
derincen <31496476+derincen@users.noreply.github.com>
2018-11-15 12:20:43 -0500

    updated aggregate power spectrum plotting on utils

--- mmd_gan/utils/power_spectrum_utils.py
+++ mmd_gan/utils/power_spectrum_utils.py
@@ -215,6 +215,89 @@ def plot_power_spec(real_cube,        # should be inverse_transformed
     return 
 
 
+# import pyfftw
+# import Pk_library as PKL
+import pandas as pd
+import h5py
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import h5py
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def moving_average(data_set, periods=3):
+    weights = np.ones(periods) / periods
+    return np.convolve(data_set, weights, mode='valid')
+
+
+def power_spectrum_np(cube, mean_raw_cube):
+    """
+    taken from: https://astronomy.stackexchange.com/questions/26431/tests-for-code-that-computes-two-point-correlation-function-of-galaxies
+    
+    cube = should be in shape [. x . x. x . x . ]
+    mean_raw_cube = is the mean of the whole cube of that redshift
+    """
+
+#     print(cube.shape)
+    nc = cube.shape[2]                # define how many cells your box has
+    boxlen = 50.0           # define length of box
+    Lambda = boxlen/4.0     # define an arbitrary wave length of a plane wave
+    dx = boxlen/nc          # get size of a cell
+
+    # create plane wave density field
+#     density_field = np.zeros((nc, nc, nc), dtype='float')
+#     for x in range(density_field.shape[0]):
+#         density_field[x,:,:] = np.cos(2*np.pi*x*dx/Lambda)
+    density_field = cube
+
+#    # get overdensity field
+#     delta = density_field/np.mean(density_field) - 1
+    delta = density_field / mean_raw_cube - 1
+
+    # get P(k) field: explot fft of data that is only real, not complex
+    delta_k = np.abs(np.fft.rfftn(delta).round())
+    Pk_field =  delta_k**2
+
+    # get 3d array of index integer distances to k = (0, 0, 0)
+    dist = np.minimum(np.arange(nc), np.arange(nc,0,-1))
+    dist_z = np.arange(nc//2+1)
+    dist *= dist
+    dist_z *= dist_z
+    dist_3d = np.sqrt(dist[:, None, None] + dist[:, None] + dist_z)
+
+    # get unique distances and index which any distance stored in dist_3d 
+    # will have in "distances" array
+    distances, _ = np.unique(dist_3d, return_inverse=True)
+
+    # average P(kx, ky, kz) to P(|k|)
+    Pk = np.bincount(_, weights=Pk_field.ravel())/np.bincount(_)
+
+    # compute "phyical" values of k
+    dk = 2*np.pi/boxlen
+    k = distances*dk
+    
+    # moving averages
+    Pk = moving_average(np.asarray(Pk), 20)
+    k = moving_average(np.asarray(k), 20)
+
+    # plot results
+#     fig = plt.figure(figsize=(9,6))
+#     ax1 = fig.add_subplot(111)
+# #     ax1.plot(k, Pk, label=r'$P(\mathbf{k})$')
+#     ax1.plot(k, np.log10(Pk), 
+#              alpha = 0.2,
+#              label=r'$log(P(\mathbf{k}))$')
+#     ax1.legend()
+#     plt.show()
+
+    return Pk, dk, k
+    
 def plot_power_spec_aggregate(real_cube,        # should be inverse_transformed
                     generated_cube,   # should be inverse_transformed
                     raw_cube_mean,    # mean of the whole raw data cube (fields=z0.0)
@@ -297,6 +380,25 @@ def plot_power_spec_aggregate(real_cube,        # should be inverse_transformed
                                                   mean_raw_cube = raw_cube_mean)
         
         
+        # CALCULATE POWER SPECTRUM OF THE REAL CUBE
+    
+    #     delta_real_cube /= np.mean(delta_real_cube,
+    #                               dtype=np.float64)
+#         delta_real_cube /= raw_cube_mean
+#         delta_real_cube -= 1.0
+#         delta_real_cube = delta_real_cube.astype(np.float32)
+
+#         Pk_real_cube = PKL.Pk(delta_real_cube, BoxSize, axis, MAS, threads)
+
+
+#         # CALCULATE POWER SPECTRUM OF THE GENERATED CUBE
+#     #     delta_gen_cube /= np.mean(delta_gen_cube,
+#     #                              dtype=np.float64)
+#         delta_gen_cube /= raw_cube_mean
+#         delta_gen_cube -= 1.0
+#         delta_gen_cube = delta_gen_cube.astype(np.float32)
+
+#         Pk_gen_cube = PKL.Pk(delta_gen_cube, BoxSize, axis, MAS, threads)
         
         k_log_series_real = np.log10(np.array(k_real))
         k_log_real.append(k_log_series_real)
@@ -307,33 +409,67 @@ def plot_power_spec_aggregate(real_cube,        # should be inverse_transformed
         Pk_log_real.append(Pk_log_series_real)
         Pk_log_series_gen = np.log10(np.array(Pk_gen))
         Pk_log_gen.append(Pk_log_series_gen)
+        
+        plt.plot(k_log_series_real, 
+                 Pk_log_series_real, 
+                 color="r", 
+                 alpha = 0.2)
+        plt.plot(k_log_series_gen, 
+                 Pk_log_series_gen, 
+                 color="b",
+                 alpha = 0.2)
     
+    # check axis and shape
+    k_log_real = np.array(k_log_real)
+    print ("klog_ shape = "+str(k_log_real.shape))
+    k_log_gen = np.array(k_log_gen)
+    print ("klog_gen shape = "+str(k_log_gen.shape))
+    Pk_log_real = np.array(Pk_log_real)
+    print ("Pklog_real shape = "+str(Pk_log_real.shape))
+    Pk_log_gen = np.array(Pk_log_gen)
+    print ("Pklog_gen shape = "+str(Pk_log_gen.shape))
     # get the means for each k value - among-cubes mean
-    k_log_means_real = np.mean(np.array(k_log_real),
+    
+    k_log_means_real = np.mean(k_log_real,
                                 axis=0)
-    k_log_means_gen = np.mean(np.array(k_log_gen),
+    
+    print ("\nk_log_means_real = "+str(k_log_means_real))
+    print ("\nk log gen whole series = "+str(k_log_gen))
+    
+    k_log_means_gen = np.mean(k_log_gen,
                                 axis=0)
     
     # ge the standard deviations for each x ax value
-    k_log_stds_real = np.std(np.array(k_log_real),
+    k_log_stds_real = np.std(k_log_real,
                                 axis=0)
     
-    k_log_stds_gen = np.std(np.array(k_log_gen),
+    print ("\nk log stds real = "+str(k_log_stds_real))
+    
+    k_log_stds_gen = np.std(k_log_gen,
                                 axis=0)
+    
+    print ("\nk_log_stds_gen = "+str(k_log_stds_gen))
     # get the means for each k value
-    Pk_log_means_real = np.mean(np.array(Pk_log_real),
+    Pk_log_means_real = np.mean(Pk_log_real,
                                 axis=0)
-    Pk_log_means_gen = np.mean(np.array(Pk_log_gen),
+    
+    print ("\nPk_log_means_real = "+str(Pk_log_means_real))
+    Pk_log_means_gen = np.mean(Pk_log_gen,
                                 axis=0)
+    
+    print ("\nPk_log_means_gen = "+str(Pk_log_means_gen))
     
     # get the standard deviations for each x ax value
-    Pk_log_stds_real = np.std(np.array(Pk_log_real),
-                                axis=0)
-    
-    Pk_log_stds_gen = np.std(np.array(Pk_log_gen),
+    Pk_log_stds_real = np.std(Pk_log_real,
                                 axis=0)
     
     
+    print ("\nPk_log_stds_real = "+str(Pk_log_stds_real))
+    
+    Pk_log_stds_gen = np.std(Pk_log_gen,
+                                axis=0)
+    
+    print ("\nPk_log_stds_gen = "+str(Pk_log_stds_gen))
     
     for i in range(len(k_log_means_gen)):
         if i % k_steps == 0:
@@ -347,13 +483,15 @@ def plot_power_spec_aggregate(real_cube,        # should be inverse_transformed
     plt.plot(k_log_means_real, 
              Pk_log_means_real, 
              color="r", 
-             alpha = 0.2,
-             label="Real Samples")
+             alpha = 1,
+             label="Real Samples",
+            linewidth=2.5)
     plt.plot(k_log_means_gen, 
              Pk_log_means_gen, 
              color="b",
-             alpha = 0.2,
-             label="Generated from Noise")
+             alpha = 1,
+             label="Generated from Noise",
+             linewidth=2.5)
         
     plt.rcParams["font.size"] = 12
     plt.title("Power Spectrum Comparison - (Red: Real, Blue: Noise-Generated)")
