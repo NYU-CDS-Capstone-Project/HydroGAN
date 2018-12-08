2c0e54a8c35c5d94a644c9b3e60f8ca20a76ba60
atakanokan <atakanokan@outlook.com>
2018-12-08 17:44:19 -0500

    uptodate plot utils

--- mmd_gan/utils/plot_utils.py
+++ mmd_gan/utils/plot_utils.py
@@ -102,20 +102,20 @@ def visualize_cube(cube=None,      # array name
 #     initial_mean = np.mean(data_1dim) - stdev_to_white*np.std(data_1dim)
 #     mask = data_1dim / raw_cube_max > 0.
 #     mask = mask.astype(np.int)
-#     """
-#     Only plot the highest 10K points
-#     """
-#     n = -25000
-#     sorted_data = np.sort(data_1dim)
-#     n_max = sorted_data[n]
-#     mask = data_1dim > n_max
-#     mask = mask.astype(np.int)    
+    """
+    Only plot the highest 10K points
+    """
+    n = -25000
+    sorted_data = np.sort(data_1dim)
+    n_max = sorted_data[n]
+    mask = data_1dim > n_max
+    mask = mask.astype(np.int)    
 
-    """
-    Plot Randomly Selected Points
-    """
-    p = 0.05 # the proportion of all points to be plotted
-    mask = np.random.binomial(n = 1, p = p, size = len(data_1dim))
+#     """
+#     Plot Randomly Selected Points
+#     """
+#     p = 0.05 # the proportion of all points to be plotted
+#     mask = np.random.binomial(n = 1, p = p, size = len(data_1dim))
 
 
     """
@@ -178,9 +178,9 @@ def visualize_cube(cube=None,      # array name
 
         cmap = colors.LinearSegmentedColormap.from_list("", ["white","blue"])
         new_cmap = truncate_colormap(cmap, 
-                                     minval = 0.5, 
+                                     minval = 0.2, 
                                      maxval = 1,
-                                     n=100)
+                                     n=10000)
 
         ## IGNORE BELOW 3D PLOT FORMATTING 
 
